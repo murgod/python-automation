e5ad0af11704abaed7720b804739462ac2e4b904
akmurgod <akmurgod@paypal.com>
2019-10-09 19:23:03 -0400

    simplified regex

--- validate_ipv4.py
+++ validate_ipv4.py
@@ -6,7 +6,7 @@ IP = sys.argv[1]
 
 
 #compile the regex to validate ipv4 address
-ipv4_pattern = re.compile("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")
+ipv4_pattern = re.compile("^(([01]?[0-9][0-9]?)\\.){3}([01]?[0-9][0-9]?)$")
 
 
 #check if IP matches all the rgex conditions
