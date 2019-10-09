66bbf6081dcdc92578fc9682f8068abea2078d67
Akshay Murgod <40592593+murgod@users.noreply.github.com>
2019-10-09 17:00:57 -0400

    script to validate IPV4 address

--- validate_ipv4.py
+++ validate_ipv4.py
@@ -1,1 +1,18 @@
+import re
+import sys
+
+#Read command line input
+IP = sys.argv[1]
+
+
+#compile the regex to validate ipv4 address
+ipv4_pattern = re.compile("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")
+
+
+#check if IP matches all the rgex conditions
+if  ipv4_pattern.match(IP):
+    print "Valid ipv4 address"
+else:
+    print "invalid ipv4 address"
+
 
